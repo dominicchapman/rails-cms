3a4a3f7988eb68a55010ba1c1c7774905791892d
Dominic Chapman <dominicchapman@me.com>
2017-08-21 22:35:09 +0100

    example of rendering a different template

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,14 @@
 class DemoController < ApplicationController
 
   layout false
-  
+
   def index
+  	render('index')
   end
+
+  # define action for hello template
+  def hello
+  	render('hello')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  
+  # root route
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/hello'
+
+  # default route, which may go away in future versions of rails.
+  # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
